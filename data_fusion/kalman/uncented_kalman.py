0d15c678fff09dadf123c3ec5caa0f783599d676
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2021-09-26 22:46:05 +0200

    mic drop

--- data_fusion/kalman/uncented_kalman.py
+++ data_fusion/kalman/uncented_kalman.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from data_fusion.kalman.extended_kalman import h_x, a_x
+from data_fusion.kalman.extended_kalman import h_x, a_x, cww as Q
 from data_fusion.utils.data_parsing import result
 
 """
@@ -40,18 +40,21 @@ def calc_sigma_points(x_pred, c_pred) -> np.ndarray:
     return sigma.T
 
 
-def calc_weights_wa():
+def calc_weights_wa() -> np.ndarray:
     """
     Calculate __first__ order weights. s.th. $\sum \eq 1$
 
     :return:
     """
-    wa = list()
+    f_wa = list()
 
-    wa.append((alpha ** 2 * k - 4) / (alpha ** 2 * k))  # 4 is a dimension
+    f_wa.append((alpha ** 2 * k - 4) / (alpha ** 2 * k))  # 4 is a dimension
     for i in range(1, 9):
-        wa.append(1. / 2 * alpha ** 2 * k)
-    return np.array(wa, dtype=float)
+        f_wa.append(1. / 2 * alpha ** 2 * k)
+    return np.array(f_wa, dtype=float)
+
+
+wa = calc_weights_wa()
 
 
 def calc_weights_wc():
@@ -60,17 +63,18 @@ def calc_weights_wc():
 
     :return:
     """
-    wa_zero = calc_weights_wa()[0]  # TODO: check if stackable
+    f_wc = list()
 
-    wc = list()
-
-    wc.append(wa_zero + 1. - alpha ** 2 + beta)
+    f_wc.append(wa[0] + 1. - alpha ** 2 + beta)
     for i in range(1, 9):
-        wc.append(1. / 2 * alpha ** 2 * k)
-    return np.array(wc, dtype=float)
+        f_wc.append(1. / 2 * alpha ** 2 * k)
+    return np.array(f_wc, dtype=float)
 
 
-def calc_a_matrix(c_pred):
+wc = calc_weights_wc()
+
+
+def calc_a_matrix(c_pred) -> np.ndarray:
     """
     Cholesky decomposition from C-prediction.
 
@@ -80,7 +84,7 @@ def calc_a_matrix(c_pred):
     return np.linalg.cholesky(c_pred)
 
 
-def _r_fn(index: int, sigma: np.ndarray):
+def _r_fn(index: int, sigma: np.ndarray) -> np.ndarray:
     row = result[index]
     ego_x = row[6][0]  # ego pose
     ego_y = row[6][1]
@@ -93,7 +97,7 @@ def _r_fn(index: int, sigma: np.ndarray):
     return np.array(lst, dtype=float)
 
 
-def calc_z(index: int, sigma: np.ndarray):
+def calc_z(index: int, sigma: np.ndarray) -> np.ndarray:
     """
     Calculate transformed points.
 
@@ -111,14 +115,13 @@ def calc_z(index: int, sigma: np.ndarray):
     return np.array(zts).reshape(9, 4)
 
 
-def calc_z_mean(index: int, sigma: np.ndarray):
+def calc_z_mean(index: int, sigma: np.ndarray) -> np.ndarray:
     """
     Empirical mean.
 
     :return:
     """
     z_calc = []
-    wa = calc_weights_wa()
     z = calc_z(index, sigma)
     for i, row in enumerate(z):
         z_calc.append(wa[i] * row)
@@ -126,25 +129,35 @@ def calc_z_mean(index: int, sigma: np.ndarray):
     return sum(z_calc)
 
 
-def calc_cross_covariance():
+def calc_cross_covariance(sigma: np.ndarray, x_pred: np.ndarray, z: np.ndarray, z_mean: np.ndarray) -> np.ndarray:
     """
     Calculate cross covariance matrix
 
     :return:
     """
-    raise Exception("Not implemented")
+
+    acc = np.zeros((4, 4))
+    zm = z_mean.reshape(4, 1)
+
+    for i, sig in enumerate(sigma.T):
+        zz = z[i].reshape(4, 1)
+        zz = zz - zm
+        m = sig.reshape(4, 1) - x_pred
+        acc += wc[i] * (m @ zz.T)
+
+    return acc
 
 
-def kalman_gain():
+def kalman_gain(cross_cov: np.ndarray, s_cov: np.ndarray) -> np.ndarray:
     """
     Calculate Kalman gain matrix
 
     :return:
     """
-    raise Exception("Not implemented")
+    return cross_cov @ np.linalg.inv(s_cov)
 
 
-def x_prepare(sigma: np.ndarray):
+def x_prepare(sigma: np.ndarray) -> np.ndarray:
     x_acc = []
     for s_row in sigma.T:
         [xx, yy, _, _] = s_row
@@ -153,8 +166,7 @@ def x_prepare(sigma: np.ndarray):
     return np.array(x_acc, dtype=float)
 
 
-def x_prediction(x_prep: np.ndarray):
-    wa = calc_weights_wa()
+def x_prediction(x_prep: np.ndarray) -> np.ndarray:
     x_acc = np.zeros((4, 1))
 
     for i, prep in enumerate(x_prep):
@@ -163,37 +175,44 @@ def x_prediction(x_prep: np.ndarray):
     return x_acc.reshape(4, 1)
 
 
-def c_prediction():
-    pass
+def c_prediction(x_prep: np.ndarray, x_predicts: np.ndarray) -> np.ndarray:
+    acc = np.zeros((4, 4))
+
+    for i, zz in enumerate(x_prep):
+        nz = zz.reshape(4, 1)
+        nz_mean = x_predicts.reshape(4, 1)
+        rz = nz - nz_mean
+        acc += wc[i] * (rz @ rz.T)
+
+    return acc + Q
 
 
-def x_update():
+def x_update(x_pred: np.ndarray, kalman: np.ndarray, z: np.ndarray, z_mean: np.ndarray) -> np.ndarray:
     """
     Update mean estimate of coordinates.
 
     :return:
     """
-    raise Exception("Not implemented")
+    return x_pred + (kalman @ (z - z_mean).T)
 
 
-def c_update():
+def c_update(kalman: np.ndarray, s_cov: np.ndarray, c_pred: np.ndarray):
     """
     Update covariance estimate.
 
     :return:
     """
 
-    raise Exception("Not implemented")
+    return c_pred - (kalman @ s_cov @ kalman.T)
 
 
-def s_covariance(z: np.ndarray, z_mean: np.ndarray):
+def s_covariance(z: np.ndarray, z_mean: np.ndarray) -> np.ndarray:
     """
     Covariance of transformed points
 
     :return:
     """
 
-    wc = calc_weights_wc()
     acc = np.zeros((4, 4))
 
     for i, zz in enumerate(z):
